23a83f2a275ccb80b91c6ef383cc8a305193603d
Aleksey Sidorov <sauron1987@gmail.com>
2024-05-08 17:54:55 +0300

    Add mostly compiled foundationdb package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@ final: prev:
 {
   criterion-table = prev.callPackage ./criterion-table.nix { };
   cargo-makedocs = prev.callPackage ./cargo-makedocs.nix { };
+  fdbPackages = prev.callPackage ./foundationdb { };
 }
