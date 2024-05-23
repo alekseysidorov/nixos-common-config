4f88101e5c2b4934ea5fcc95c9d2dfd9fa830a5d
Aleksey Sidorov <sauron1987@gmail.com>
2024-05-24 00:56:11 +0300

    Remove something old

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -13,18 +13,5 @@
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
     wireplumber.enable = true;
-
-    # See this article
-    # https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
-    # wireplumber.configPackages = [
-    #   (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-    #     		bluez_monitor.properties = {
-    #     			["bluez5.enable-sbc-xq"] = true,
-    #     			["bluez5.enable-msbc"] = true,
-    #     			["bluez5.enable-hw-volume"] = true,
-    #     			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    #     		}
-    #     	'')
-    # ];
   };
 }
