d4ab2cf33d0d9e750b27df9a784ff7ba1ae7c5f1
Aleksey Sidorov <sauron1987@gmail.com>
2024-07-24 00:39:23 +0300

    Add fresh zed editor

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@ final: prev:
 {
   criterion-table = prev.callPackage ./criterion-table.nix { };
   cargo-makedocs = prev.callPackage ./cargo-makedocs.nix { };
+  zed-editor = prev.callPackage ./zed-editor {};
 }
