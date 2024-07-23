12b70989c22b62bd2bdd4ba405659f10f2ce7923
Aleksey Sidorov <sauron1987@gmail.com>
2024-07-24 01:27:16 +0300

    No way

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,4 @@ final: prev:
 {
   criterion-table = prev.callPackage ./criterion-table.nix { };
   cargo-makedocs = prev.callPackage ./cargo-makedocs.nix { };
-  zed-editor = prev.callPackage ./zed-editor { };
 }
