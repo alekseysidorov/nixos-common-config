57776ceabb836e5c0a6e0b8b41ebcd039ef4c736
Aleksey Sidorov <sauron1987@gmail.com>
2024-12-03 14:38:31 +0300

    It doesn't work

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,16 +1,4 @@
 final: prev:
-
-let
-  isLinux = prev.stdenv.targetPlatform.isLinux;
-in
 {
   criterion-table = prev.callPackage ./criterion-table.nix { };
-  # Special case for Zed on linux
-  zed-editor =
-    if isLinux then
-      prev.zed-editor.fhsWithPackages
-        (pkgs:
-          with pkgs; [ zlib openssl ]
-        )
-    else prev.zed-editor;
 }
