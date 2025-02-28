6530568d98103d2b1921b7c3bfebc029762e12e4
Aleksey Sidorov <sauron1987@gmail.com>
2025-02-28 16:58:31 +0300

    Update git config

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -15,7 +15,37 @@
       alias.cln = "!git clean -dxf -e \"/.vscode\" -e \".idea\"";
       alias.sweep-branches = "!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done";
 
-      push.autoSetupRemote = true;
+      # Some settings from this article 
+      # 
+      # https://habr.com/en/articles/886538/
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+        followTags = true;
+      };
+
+      fetch = {
+        prune = true;
+        followTags = true;
+        all = true;
+      };
+
+      help.autocorrect = "prompt";
+      diff.algorithm = "histogram";
+
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+
+      pull.rebase = "true";
+
     }];
   };
 
