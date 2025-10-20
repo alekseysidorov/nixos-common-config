5c30dd3bb3f3231bd6663c9462a91ab2471ade6b
Aleksey Sidorov <sauron1987@gmail.com>
2025-10-20 19:40:16 +0300

    Improve pw config

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -17,28 +17,13 @@
     jack.enable = true;
     wireplumber.enable = true;
 
-    # Low latency timing configuration
-    extraConfig.pipewire."context.properties" = {
-      "default.clock.rate" = 48000;
-      "default.clock.allowed-rates" = [ 44100 48000 ];
-      "default.clock.quantum" = 32; # попробуешь 16, если тянет
-      "default.clock.min-quantum" = 16;
-      "default.clock.max-quantum" = 1024;
-      "default.clock.force-quantum" = 32;
-      "default.clock.force-rate" = 48000;
-    };
-
-    extraConfig.pipewire."context.rt" = {
-      "rt.prio" = 95;
-      "rt.time.soft" = 200000;
-      "rt.time.hard" = 200000;
-      "nice.level" = -15;
-    };
-
-    # Fine tuning for compa tibility with pulseudio applications
-    extraConfig.pipewire-pulse."context.properties" = {
-      "api.alsa.period-size" = 16;
-      "api.alsa.headroom" = 0;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+      };
     };
   };
 }
