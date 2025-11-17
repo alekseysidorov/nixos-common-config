cc08cd2e80bcd150fd5c347b0979a3bf4697b296
Aleksey Sidorov <sauron1987@gmail.com>
2025-11-17 18:57:52 +0300

    Use vim as core editor

--- modules/home/core.nix
+++ modules/home/core.nix
@@ -7,9 +7,14 @@
     ripgrep
     pwgen
     jq
+    vim
   ];
 
   programs.vim = {
     extraConfig = lib.mkDefault (builtins.readFile ./assets/vimrc);
   };
+
+  home.sessionVariables = lib.mkMerge [{
+    EDITOR = "vim";
+  }];
 }
